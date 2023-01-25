cb0905df195fbd73bb5f1d1703c126b4ba601af4
Jonathan Tsai <jontsai@users.noreply.github.com>
2023-01-24 16:35:01 -0800

    ✨ Handles GitHub Enterprise Cloud as well as public GitHub.com - 💄 Also detects and rewrites JIRA tags inside of `commit-title` class name - 🧑‍💻🔨 Adds `.prettierrc` and `.editorconfig` for developer QoL - ⚡️ Rewrites as VanillaJS (faster, less overhead) - 🔥 Removes vendored jQuery and Lodash - 🚚 Renames `LICENSE` to `LICENSE.md` - 📦 Updates `make package` target to read in `FILES` and write package with version name automatically read from `manifest.json` - 🔖 Version bump: v1.2.0

--- src/js/github_jira.js
+++ src/js/github_jira.js
@@ -1,44 +1,49 @@
-$(function() {
-    console.log("GitHub JIRA Issue Links Rewriter Extension is loaded");
+if (window.location.hostname.match(/^.*git.*$/)) {
+    (function () {
+        console.log('GitHub JIRA Issue Links Rewriter Extension is loaded');
 
-    var GITHUB_COMMENT_TAG_SELECTORS = [
-        '.js-issue-title',
-        '.comment-body'
-    ];
-    var jiraIssueBaseUrl = '';
+        const GITHUB_COMMENT_TAG_SELECTORS = [
+            'js-issue-title',
+            'comment-body',
+            'commit-title',
+        ];
+        let jiraIssueBaseUrl = '';
 
-    function scanAndReplaceJIRAIssueTags() {
-        _.each(GITHUB_COMMENT_TAG_SELECTORS, function(tagSelector) {
-            var elements = $(tagSelector);
-            _.each(elements, function(element) {
-                var text = $(element).html();
-                var rewritten = text.replace(
-                    /\[([a-zA-Z]+)-(\d+)\]/,
-                    function(match, issuePrefix, issueNumber) {
-                        var issueId = issuePrefix + '-' + issueNumber;
-                        var issueUrl = jiraIssueBaseUrl + '/' + issueId;
-                        var replacement = '[<a href="' + issueUrl + '" target="_blank">' + issueId + '</a>]';
-                        return replacement;
-                    }
-                );
-                $(element).html(rewritten);
+        function scanAndReplaceJIRAIssueTags() {
+            GITHUB_COMMENT_TAG_SELECTORS.forEach((tagSelector) => {
+                const elements = document.querySelectorAll(`.${tagSelector}`);
+                elements.forEach((element) => {
+                    const text = element.innerHTML;
+                    const rewritten = text.replace(
+                        /\[([a-zA-Z]+)-(\d+)\]/,
+                        function (match, issuePrefix, issueNumber) {
+                            const issueId = issuePrefix + '-' + issueNumber;
+                            const issueUrl = jiraIssueBaseUrl + '/' + issueId;
+                            const replacement = `[<a href="${issueUrl}" target="_blank">${issueId}</a>]`;
+                            return replacement;
+                        }
+                    );
+                    element.innerHTML = rewritten;
+                });
             });
-        });
-    }
+        }
 
-    function initEventHandlers() {
-    }
+        function initEventHandlers() {}
 
-    function init() {
-        chrome.storage.sync.get({
-            jiraIssueBaseUrl: ''
-        }, function(items) {
-            jiraIssueBaseUrl = items.jiraIssueBaseUrl;
-            console.log('JIRA Issue Base URL: ', jiraIssueBaseUrl);
-            scanAndReplaceJIRAIssueTags();
-        });
-    }
+        function init() {
+            chrome.storage.sync.get(
+                {
+                    jiraIssueBaseUrl: '',
+                },
+                function (items) {
+                    jiraIssueBaseUrl = items.jiraIssueBaseUrl;
+                    console.log('JIRA Issue Base URL: ', jiraIssueBaseUrl);
+                    scanAndReplaceJIRAIssueTags();
+                }
+            );
+        }
 
-    initEventHandlers();
-    init();
-});
+        initEventHandlers();
+        init();
+    })();
+}
